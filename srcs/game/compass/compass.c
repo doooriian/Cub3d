344ac7dd111ee8baee163e2df16d9106217d2921
rcaillie <rcaillie@student.42angouleme.fr>
2025-05-13 16:31:40 +0200

    Ajout du système de boussole (compass)

---
+++ srcs/game/compass/compass.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compass.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcaillie <rcaillie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 15:27:54 by rcaillie          #+#    #+#             */
+/*   Updated: 2025/05/13 16:12:15 by rcaillie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+#define CROSS_SIZE 50
+#define BASE_WIDTH 10
+#define COLOR_NORTH 0xFF0000
+#define COLOR_OTHER 0xFFFFFF
+
+
+
+void	draw_arrow(t_game *game, float angle, int color, t_point point)
+{
+	t_point	tip;
+	t_point	base1;
+	t_point	base2;
+
+	tip = get_point(angle, CROSS_SIZE, point);
+	base1 = get_point(angle + M_PI / 2, BASE_WIDTH, point);
+	base2 = get_point(angle - M_PI / 2, BASE_WIDTH, point);
+	draw_filled_triangle(game, (t_triangle){tip, base1, base2}, color);
+}
+
+static void	draw_compass_arrows(t_game *game, int cx, int cy, float base_angle)
+{
+	int		i;
+	float	angle;
+	int		color;
+	t_point	center;
+
+	center.x = cx;
+	center.y = cy;
+	i = 0;
+	while (i < 4)
+	{
+		angle = base_angle + i * (M_PI / 2);
+		if (i == 0)
+			color = COLOR_NORTH;
+		else
+			color = COLOR_OTHER;
+		draw_arrow(game, angle, color, center);
+		i++;
+	}
+}
+
+void	draw_compass(t_game *game)
+{
+	int		cx;
+	int		cy;
+	float	base_angle;
+
+	cx = WIDTH - CROSS_SIZE - 20;
+	cy = CROSS_SIZE + 20;
+	base_angle = game->player.angle;
+	draw_compass_arrows(game, cx, cy, base_angle);
+}
