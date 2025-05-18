d2c363da869c0d70b91f858002018a5f6a9f78a8
doooriian <dorian.ley42@gmail.com>
2025-05-18 14:56:49 +0200

    Refactor texture retrieval functions for norm

--- srcs_bonus/game/wall_rendering_utils.c
+++ srcs_bonus/game/wall_rendering_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wall_rendering_utils.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcaillie <rcaillie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: doley <doley@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 23:18:22 by rcaillie          #+#    #+#             */
-/*   Updated: 2025/05/16 19:10:39 by rcaillie         ###   ########.fr       */
+/*   Updated: 2025/05/18 14:53:52 by doley            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,16 +50,13 @@ void	draw_line(t_game *game, int x, t_point draw_s_e, int color)
 	}
 }
 
-t_img	*get_texture(t_game *game, t_ray *ray)
+static t_img	*get_sprite_texture(t_game *game, t_ray *ray, int i)
 {
-	int i;
-
-	i = 0;	// PAS OTPIMISE !!!!!!!!!!!!!!!!!!!!!!!!!!! UTILISER MAP DOORS ET AJOUTER SPRITES DEDANS 
 	while (i < game->sprite_count)
 	{
-		if (game->sprites[i].active &&
-			game->sprites[i].x == ray->map_x &&
-			game->sprites[i].y == ray->map_y)
+		if (game->sprites[i].active
+			&& game->sprites[i].x == ray->map_x
+			&& game->sprites[i].y == ray->map_y)
 		{
 			if (game->sprites[i].frame == 0)
 				return (&game->imgs.sprite1);
@@ -70,6 +67,18 @@ t_img	*get_texture(t_game *game, t_ray *ray)
 		}
 		i++;
 	}
+	return (NULL);
+}
+
+t_img	*get_texture(t_game *game, t_ray *ray)
+{
+	int		i;
+	t_img	*sprite;
+
+	i = 0;
+	sprite = get_sprite_texture(game, ray, i);
+	if (sprite)
+		return (sprite);
 	if (game->map_data.map[ray->map_y][ray->map_x] == 'D')
 	{
 		if (game->map_doors && game->map_doors[ray->map_y][ray->map_x] == '1')
@@ -83,3 +92,37 @@ t_img	*get_texture(t_game *game, t_ray *ray)
 		return (&game->imgs.no);
 	return (&game->imgs.so);
 }
+
+// t_img	*get_texture(t_game *game, t_ray *ray)
+// {
+// 	int i;
+
+// 	i = 0;	// PAS OTPIMISE !!!!!!!!!!!!!!!!!!!!!!!!!!! UTILISER MAP DOORS ET AJOUTER SPRITES DEDANS
+// 	while (i < game->sprite_count)
+// 	{
+// 		if (game->sprites[i].active &&
+// 			game->sprites[i].x == ray->map_x &&
+// 			game->sprites[i].y == ray->map_y)
+// 		{
+// 			if (game->sprites[i].frame == 0)
+// 				return (&game->imgs.sprite1);
+// 			if (game->sprites[i].frame == 1)
+// 				return (&game->imgs.sprite2);
+// 			if (game->sprites[i].frame == 2)
+// 				return (&game->imgs.sprite3);
+// 		}
+// 		i++;
+// 	}
+// 	if (game->map_data.map[ray->map_y][ray->map_x] == 'D')
+// 	{
+// 		if (game->map_doors && game->map_doors[ray->map_y][ray->map_x] == '1')
+// 			return (&game->imgs.door);
+// 	}
+// 	if (ray->side == 0 && ray->dir_x < 0)
+// 		return (&game->imgs.we);
+// 	if (ray->side == 0 && ray->dir_x >= 0)
+// 		return (&game->imgs.ea);
+// 	if (ray->side == 1 && ray->dir_y < 0)
+// 		return (&game->imgs.no);
+// 	return (&game->imgs.so);
+// }
